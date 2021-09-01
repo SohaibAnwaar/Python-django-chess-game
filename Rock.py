7afeaf5c8661c2afb95d27cbc366f6a84ebcf355
Farhan Pirzada <farhan.pirzada@invozone.com>
2021-09-01 22:03:22 +0500

    commenting in the classes

--- Rock.py
+++ Rock.py
@@ -4,8 +4,19 @@ from Piece import Piece
 
 class Rock(Piece):
 
+    """
+    Rock class is inherit from Piece
+    """
+
     def canMove(self,board,x,y):
+
+        """
+        param x : take the target x-axis coordinate
+        param y : take the target y-axis coordinate
+        """
+
         sourceX,sourceY,targetX,targetY=self.x,self.y,x,y
+        # check only rock can move in four directions forward, backward, left, right
         if sourceX == targetX or sourceY == targetY:
             return True
         else:
